b43d93f567f1642552ba8da79aa81e52cc880930
artem_shinkevich <SKILLBOX2021oNe>
2022-11-18 21:44:34 +0300

    All done

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
+        int first = 500;
+        boolean moreThemFirst = from1UpTo1000 < first;
+
+        if (moreThemFirst) {
+            System.out.println("Eagle");
+        } else {
+            System.out.println("Tail");
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        boolean greatest = first >= second;
+        if (greatest) {
+            System.out.println(first);
+        } else {
+            System.out.println(second);
+        }
     }
 }
